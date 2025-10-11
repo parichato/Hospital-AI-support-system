62fb2b676635a862d8cf380468c7f60d53613d73
parichato <parichart.tuiwong@gmail.com>
2025-10-11 18:53:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 from catboost import CatBoostClassifier
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 # ----------------------------------------------------------
 # ⚙️ Page Setup
